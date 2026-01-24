12c8c8d205d7e49362a7b82d511c6591f69e4118
Alex <alexandriaduell@gmail.com>
2026-01-23 22:09:53 -0500

    Added insert sql data for the people table

---
+++ db/inserts/1_people.sql
@@ -0,0 +1,8 @@
+INSERT INTO people(first_name, last_name, gender, dob, email, phone_num, street_address_1, street_address_2, city, state, country, zip_code)
+VALUES ('Alex', 'Adams', 'female', '1996-03-05', 'alexadams@email.com', '987-547-4251', '6827 Glenwood Ave', null, 'Raleigh', 'NC', 'USA', '27603'),
+('Casey', 'Martinez', 'male', '2001-03-25', 'casey.martinez@example.com', NULL, '789 Pine Rd', 'Apt 5C', 'Chicago', 'IL', 'USA', '60616'),
+('Evan', 'Cole', NULL, '2010-12-01', NULL, NULL, NULL, NULL, NULL, 'NC', 'USA', NULL),
+('Dylan', 'Moore', 'male', '2012-11-13', NULL, '336-555-4419', NULL, NULL, NULL, 'TX', 'USA', NULL),
+('Natalie', 'Perez', 'female', '1993-07-27', 'natalie.perez@email.com', '305-555-9183', '670 Coral Way', NULL, 'Miami', 'FL', 'USA', '33145'),
+('Quinn', 'Foster', 'prefer not to say', '1987-10-18', NULL, NULL, NULL, NULL, NULL, NULL, 'USA', NULL),
+('Aisling', 'OConnor', 'female', '1999-02-09', 'aoife.oconnor@example.ie', NULL, '3 Trinity Lane', NULL, 'Dublin', NULL, 'Ireland', 'D02 XY76');
