4c401ee7473bc80486a7814ba02a0e94018bc924
Alex <alexandriaduell@gmail.com>
2026-02-04 13:27:50 -0500

    Added data to player matches insert file

---
+++ db/inserts/7_player_matches.sql
@@ -0,0 +1,7 @@
+INSERT INTO matches(player_id, match_id, starting_side, player_order)
+VALUES (1, 1, 'left', 'player_1'),
+(3, 1, 'right', 'player_2'),
+(5, 2, 'left', 'player_1'),
+(7, 2, 'right', 'player_2'),
+(2, 3, 'left', 'player_1'),
+(6, 3, 'right', 'player_2');
