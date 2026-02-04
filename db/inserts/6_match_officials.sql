aec8fbf158b6c3cdc16be4d70b5ab2ac951540a9
Alex <alexandriaduell@gmail.com>
2026-02-04 16:30:33 -0500

    Added match officials sample data for match 1 (sets with ids: 1, 2, + 3)

---
+++ db/inserts/6_match_officials.sql
@@ -0,0 +1,7 @@
+INSERT INTO match_officials (official_person_id, set_id, official_type)
+VALUES (6, 1, 'referee'),
+(6, 2, 'referee'),
+(7, 3, 'referee'),
+(7, 1, 'witness'),
+(7, 2, 'witness'),
+(6, 3, 'witness');
