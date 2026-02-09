019793be5b84683418e971744c40e26b8d762956
Alex <alexandriaduell@gmail.com>
2026-02-09 00:35:25 -0500

    Added dml file with commented out code for the description + what CRUD statements will be made for the tables

---
+++ DML.sql
@@ -0,0 +1,69 @@
+-- Competitive Air Hockey Database
+-- Alexandria Duell and Rita Berglund (House of Pucks)
+
+-- This file contains DML (Data Manipulation Language) to apply CREATE, READ, UPDATE, + DELETE (CRUD) queries to the tables
+
+
+------------
+-- people --
+------------
+-- There is no DELETE for this table, as the community wants to keep data integrity for past matches
+-- CREATE
+-- READ
+-- UPDATE
+
+---------------
+-- locations --
+---------------
+-- There is no DELETE for this table, as the community wants to keep data integrity for past matches
+-- CREATE
+-- READ
+-- UPDATE
+
+-------------
+-- matches --
+-------------
+-- CREATE
+-- READ
+-- UPDATE
+-- DELETE
+
+----------
+-- sets --
+----------
+-- CREATE
+-- READ
+-- UPDATE
+-- DELETE
+
+-----------
+-- games --
+-----------
+-- CREATE
+-- READ
+-- UPDATE
+-- DELETE
+
+---------------------
+-- match_officials --
+---------------------
+-- CREATE
+-- READ
+-- UPDATE
+-- DELETE
+
+--------------------
+-- player_matches --
+--------------------
+-- CREATE
+-- READ
+-- UPDATE
+-- DELETE
+
+----------------------
+-- people_locations --
+----------------------
+-- CREATE
+-- READ
+-- UPDATE
+-- DELETE
