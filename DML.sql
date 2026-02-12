60d2c838d20a343843a42aa131790f7d83242347
Alex <alexandriaduell@gmail.com>
2026-02-12 14:08:39 -0500

    Updated locations with owners view to include id and put location id + name before the owner, and then the rest of the location data

--- DML.sql
+++ DML.sql
@@ -50,9 +50,10 @@ VALUES (table_qty, email, phone_num, street_address_1, street_address_2, city, s
 ----- READ -----
 -- Get all data
 CREATE OR REPLACE VIEW locations_with_owners AS
-SELECT CONCAT(p.first_name, ' ', p.last_name) as owner, 
+SELECT l.location_id, l.location_name, 
+        CONCAT(p.first_name, ' ', p.last_name) as owner, 
     	l.table_qty, l.email, l.phone_num, l.street_address_1, l.street_address_2, 
-        l.city, l.state, l.country, l.zip_code, l.type_of_address, l.location_name, l.notes
+        l.city, l.state, l.country, l.zip_code, l.type_of_address, l.notes
         from locations as l 
 JOIN people_locations as pl on pl.location_id = l.location_id 
 INNER JOIN people as p on p.person_id = pl.person_id;
