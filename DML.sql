afc97b562133656630c8cdb5959b91e2d30c976f
Alex <alexandriaduell@gmail.com>
2026-02-12 17:03:12 -0500

    Updated locations with owners view + select statements

--- DML.sql
+++ DML.sql
@@ -71,15 +71,15 @@ VALUES (:table_qty, :email, :phone_num, :street_address_1, :street_address_2, :c
 -- Get all data
 CREATE OR REPLACE VIEW locations_with_owners AS
 SELECT l.location_id, l.location_name, 
-        CONCAT(p.first_name, ' ', p.last_name) as owner, 
+        CONCAT(p.first_name, ' ', p.last_name) as `owner`, 
     	l.table_qty, l.email, l.phone_num, l.street_address_1, l.street_address_2, 
         l.city, l.state, l.country, l.zip_code, l.type_of_address, l.notes
         from locations as l 
 JOIN people_locations as pl on pl.location_id = l.location_id 
-INNER JOIN people as p on p.person_id = pl.person_id;
+INNER JOIN people as p on p.person_id = pl.person_id
+ORDER BY l.location_name;
 
-SELECT * FROM locations_with_owners
-ORDER BY location_name;
+SELECT * FROM locations_with_owners;
 
 ----- UPDATE -----
 UPDATE locations 
