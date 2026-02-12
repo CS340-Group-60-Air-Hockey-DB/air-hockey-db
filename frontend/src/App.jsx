a6362c2807c94cc311c113336977083cba9e24e3
Rita Berglund <andoberr@oregonstate.edu>
2026-02-11 21:30:15 -0800

    hardcoded backend port in app.jsx and specified port in vite.config.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import People from './pages/People';
 import Navigation from './components/Navigation';
 
 // Define the backend port and URL for API requests
-const backendPort = process.env.PORT_BACKEND || 63729;  // Use the port you assigned to the backend server, this would normally go in a .env file
+const backendPort = 63729;  // Use the port you assigned to the backend server, this would normally go in a .env file
 const backendURL = `http://classwork.engr.oregonstate.edu:${backendPort}`;
 
 function App() {
