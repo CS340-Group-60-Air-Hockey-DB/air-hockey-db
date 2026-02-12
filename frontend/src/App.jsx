3c9614dd1a87e5b054207e6b74029c806802b30e
Alex <alexandriaduell@gmail.com>
2026-02-12 00:28:04 -0500

    Updated so be + fe apps can read env files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,11 +15,10 @@ import MatchOfficials from './pages/Match_Officials';
 import Navigation from './components/Navigation';
 
 // Define the backend port and URL for API requests
-const backendPort = 63729;  // Use the port you assigned to the backend server, this would normally go in a .env file
+const backendPort = import.meta.env.PORT_BACKEND || 63729;  // Use the port you assigned to the backend server, this would normally go in a .env file
 const backendURL = `http://classwork.engr.oregonstate.edu:${backendPort}`;
 
 function App() {
-
     return (
         <>
             <Navigation />
