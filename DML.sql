be93d78d022be2818de52d9f7aea500324b00157
Alex <alexandriaduell@gmail.com>
2026-02-12 15:12:05 -0500

    Added colons to variables in people CRUD queries

--- DML.sql
+++ DML.sql
@@ -9,7 +9,7 @@
 ------------
 ----- CREATE -----
 INSERT INTO people (first_name, last_name, gender, dob, email, phone_num, street_address_1, street_address_2, city, state, country, zip_code)
-VALUES (first_name, last_name, gender, dob, email, phone_num, street_address_1, street_address_2, city, state, country, zip_code);
+VALUES (:first_name, :last_name, :gender, :dob, :email, :phone_num, :street_address_1, :street_address_2, :city, :state, :country, :zip_code);
 
 ----- READ -----
 -- Get all data, order by first name
@@ -18,23 +18,23 @@ ORDER BY first_name;
 
 -- Get data by id
 SELECT * from people
-WHERE person_id = person_id;
+WHERE person_id = :person_id;
 
 ----- UPDATE -----
 UPDATE people
-SET first_name = first_name, 
-    last_name = last_name, 
-    gender = gender, 
-    dob = dob, 
-    email = email, 
-    phone_num = phone_num, 
-    street_address_1 = street_address_1, 
-    street_address_2 = street_address_2, 
-    city = city, 
-    state = state, 
-    country = country, 
-    zip_code = zip_code
-WHERE person_id = person_id;
+SET first_name = :first_name, 
+    last_name = :last_name, 
+    gender = :gender, 
+    dob = :dob, 
+    email = :email,
+    phone_num = :phone_num,
+    street_address_1 = :street_address_1,
+    street_address_2 = :street_address_2,
+    city = :city,
+    `state` = :`state`,
+    country = :country
+    zip_code = :zip_code
+WHERE person_id = :person_id;
 
 ----- DELETE -----
 -- There is no DELETE for this table, as the community wants to keep data integrity for past matches
