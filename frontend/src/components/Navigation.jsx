65b004e0684180bb892652d2a2a8f28d76340734
Rita Berglund <andoberr@oregonstate.edu>
2026-02-11 20:33:23 -0800

    updated navigation page

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -4,6 +4,8 @@ function Navigation() {
             Navigation:
             <a href="/">Home</a>
             <a href="/people">Air Hockey Players</a>
+            <a href="/locations">Locations</a>
+            <a href="/matches">Matches</a>
         </nav>
     )
 } export default Navigation;
