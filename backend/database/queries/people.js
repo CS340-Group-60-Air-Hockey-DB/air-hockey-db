31f6d33b80a038556c5ee53cfb0e63a115905eaa
Alex <alexandriaduell@gmail.com>
2026-02-16 01:54:02 -0500

    Added insert person into people queries

--- backend/database/queries/people.js
+++ backend/database/queries/people.js
@@ -1,6 +1,10 @@
 const people_queries = {
     select_all: 'SELECT * from people ORDER BY first_name;',
     get_by_id: 'SELECT * from people WHERE person_id = :person_id;',
+    insert_person: `
+        INSERT INTO people (first_name, last_name, gender, dob, email, phone_num, street_address_1, street_address_2, city, state, country, zip_code)
+        VALUES (:first_name, :last_name, :gender, :dob, :email, :phone_num, :street_address_1, :street_address_2, :city, :state, :country, :zip_code);
+    `
     update_person_by_id: `
         UPDATE people
         SET first_name = :first_name, 
@@ -17,7 +21,6 @@ const people_queries = {
             zip_code = :zip_code
         WHERE person_id = :person_id;
     `,
-
 }
 
 export default people_queries
