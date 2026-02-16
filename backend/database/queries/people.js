eca160ae48613fd20d87e4cedb49edccbbdcf07d
Alex <alexandriaduell@gmail.com>
2026-02-16 01:52:15 -0500

    Added all people queries from DML into people queries

--- backend/database/queries/people.js
+++ backend/database/queries/people.js
@@ -1,6 +1,23 @@
 const people_queries = {
     select_all: 'SELECT * from people ORDER BY first_name;',
-    get_by_id: 'SELECT * from people WHERE person_id = : person_id;'
+    get_by_id: 'SELECT * from people WHERE person_id = :person_id;',
+    update_person_by_id: `
+        UPDATE people
+        SET first_name = :first_name, 
+            last_name = :last_name, 
+            gender = :gender, 
+            dob = :dob, 
+            email = :email,
+            phone_num = :phone_num,
+            street_address_1 = :street_address_1,
+            street_address_2 = :street_address_2,
+            city = :city,
+            "state" = :"state",
+            country = :country,
+            zip_code = :zip_code
+        WHERE person_id = :person_id;
+    `,
+
 }
 
 export default people_queries
