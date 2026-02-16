61b8e91eddb8b941abeb82bc4a30d9a36190265b
Alex <alexandriaduell@gmail.com>
2026-02-16 02:12:29 -0500

    Added sets queries

---
+++ backend/database/queries/sets.js
@@ -0,0 +1,30 @@
+const sets_queries = {
+    delete_by_id: `
+        DELETE FROM "sets"
+        WHERE set_id = :set_id;
+    `,
+    select_all: `
+        SELECT * from "sets"
+        ORDER BY match_id;
+    `,
+    select_by_id: `
+        SELECT * from "sets"
+        WHERE set_id = :set_id
+    `,
+    insert_set: `
+        INSERT INTO "sets"(match_id, winner_id, set_num, start_datetime, end_datetime, set_status)
+        VALUES (:match_id, :winner_id, :set_num, :start_datetime, :end_datetime, :set_status);
+    `,
+    update_by_id: `
+        UPDATE "sets"
+        SET match_id = :match_id,
+            winner_id = :winner_id,
+            set_num = :set_num,
+            start_datetime = :start_datetime,
+            end_datetime = :end_datetime,
+            set_status = :set_status
+        WHERE set_id = :set_id;
+    `,
+}
+
+export default sets_queries
