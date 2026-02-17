22d38c9c49eea52d5b627622bb151e41c450bdb1
Alex <alexandriaduell@gmail.com>
2026-02-17 02:11:00 -0600

    changed class to className to address console error

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,14 +1,14 @@
 function Navigation({location}) {
     return (
         <nav id="app-nav">
-            <a class={location === '/' ? 'highlight': ''} href="/">Home</a>
-            <a class={location === '/people' ? 'highlight': ''} href="/people">Community</a>
-            <a class={location === '/locations' ? 'highlight': ''} href="/locations">Locations</a>
-            <a class={location === '/matches' ? 'highlight': ''} href="/matches">Matches</a>
-            <a class={location === '/player_matches' ? 'highlight': ''} href="/player_matches">Player Matches</a>
-            <a class={location === '/match_officials' ? 'highlight': ''} href="/match_officials">Match Officials</a>
-            <a class={location === '/sets' ? 'highlight': ''} href="/sets">Sets</a>
-            <a class={location === '/games' ? 'highlight': ''} href="/games">Games</a>
+            <a className={location === '/' ? 'highlight': ''} href="/">Home</a>
+            <a className={location === '/people' ? 'highlight': ''} href="/people">Community</a>
+            <a className={location === '/locations' ? 'highlight': ''} href="/locations">Locations</a>
+            <a className={location === '/matches' ? 'highlight': ''} href="/matches">Matches</a>
+            <a className={location === '/player_matches' ? 'highlight': ''} href="/player_matches">Player Matches</a>
+            <a className={location === '/match_officials' ? 'highlight': ''} href="/match_officials">Match Officials</a>
+            <a className={location === '/sets' ? 'highlight': ''} href="/sets">Sets</a>
+            <a className={location === '/games' ? 'highlight': ''} href="/games">Games</a>
         </nav>
     )
 } export default Navigation;
