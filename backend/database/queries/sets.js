00e89ecb96e2fe3de0095964d2519f6ba9041c6f
Alex <alexandriaduell@gmail.com>
2026-02-19 12:33:09 -0500

    Updated queries for FE tables

--- backend/database/queries/sets.js
+++ backend/database/queries/sets.js
@@ -1,22 +1,29 @@
 const set_queries = {
     delete_by_id: `
-        DELETE FROM "sets"
+        DELETE FROM sets
         WHERE set_id = :set_id;
     `,
     select_all: `
-        SELECT * from "sets"
+        SELECT match_id,
+            CONCAT(p.first_name, ' ', p.last_name) as winner, 
+            set_num,
+            start_datetime, 
+            end_datetime,
+            set_status as status
+        from sets
+        JOIN people as p on p.person_id = sets.winner_id
         ORDER BY match_id;
     `,
     select_by_id: `
-        SELECT * from "sets"
+        SELECT * from sets
         WHERE set_id = :set_id
     `,
     insert_set: `
-        INSERT INTO "sets"(match_id, winner_id, set_num, start_datetime, end_datetime, set_status)
+        INSERT INTO sets(match_id, winner_id, set_num, start_datetime, end_datetime, set_status)
         VALUES (:match_id, :winner_id, :set_num, :start_datetime, :end_datetime, :set_status);
     `,
     update_by_id: `
-        UPDATE "sets"
+        UPDATE sets
         SET match_id = :match_id,
             winner_id = :winner_id,
             set_num = :set_num,
