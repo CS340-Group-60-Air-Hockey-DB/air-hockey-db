dd6cc215c29825e72e125209ab492193348f759c
Alex <alexandriaduell@gmail.com>
2026-02-23 23:54:08 -0500

    Added reset button to navigation with an alert; does not work to reset the database

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -5,14 +5,21 @@ function Navigation() {
 
     return (
         <nav id="app-nav">
-            <a className={pathname === '/' ? 'highlight': ''} href="/">Home</a>
-            <a className={pathname === '/people' ? 'highlight': ''} href="/people">Community</a>
-            <a className={pathname === '/locations' ? 'highlight': ''} href="/locations">Locations</a>
-            <a className={pathname === '/matches' ? 'highlight': ''} href="/matches">Matches</a>
-            <a className={pathname === '/player_matches' ? 'highlight': ''} href="/player_matches">Player Matches</a>
-            <a className={pathname === '/match_officials' ? 'highlight': ''} href="/match_officials">Match Officials</a>
-            <a className={pathname === '/sets' ? 'highlight': ''} href="/sets">Sets</a>
-            <a className={pathname === '/games' ? 'highlight': ''} href="/games">Games</a>
+            <a className={location === '/' ? 'highlight': ''} href="/">Home</a>
+            <a className={location === '/people' ? 'highlight': ''} href="/people">Community</a>
+            <a className={location === '/locations' ? 'highlight': ''} href="/locations">Locations</a>
+            <a className={location === '/matches' ? 'highlight': ''} href="/matches">Matches</a>
+            <a className={location === '/player_matches' ? 'highlight': ''} href="/player_matches">Player Matches</a>
+            <a className={location === '/match_officials' ? 'highlight': ''} href="/match_officials">Match Officials</a>
+            <a className={location === '/sets' ? 'highlight': ''} href="/sets">Sets</a>
+            <a className={location === '/games' ? 'highlight': ''} href="/games">Games</a>
+            <a 
+                onClick={() => {
+                    window.alert('Are you sure you want to reset the whole database?')
+                }}
+            >
+                Reset All Data
+            </a>
         </nav>
     )
 } export default Navigation;
