842616dc8b35648b185750480b6a2d00fb9b8f10
Alex <alexandriaduell@gmail.com>
2026-03-02 17:14:49 -0500

    Updated match_official queries to have '?' instead of colon variables (ex: ':match_official_id')

--- backend/database/queries/match_officials.js
+++ backend/database/queries/match_officials.js
@@ -1,7 +1,7 @@
 const match_official_queries = {
     delete_by_id: `
         DELETE FROM match_officials
-        WHERE match_official_id = :match_official_id;
+        WHERE match_official_id = ?;
     `,
     select_all: `
         SELECT match_official_id, 
@@ -17,14 +17,14 @@ const match_official_queries = {
     `,
     insert_official: `
         INSERT INTO match_officials (official_person_id, set_id, official_type)
-        VALUES (:official_person_id, :set_id, :official_type);
+        VALUES (?, ?, ?);
     `,
     update_by_id: `
         UPDATE match_officials
-        SET official_person_id = :official_person_id,
-            set_id = :set_id,
-            official_type = :official_type
-        WHERE match_official_id = :match_official_id;
+        SET official_person_id = ?,
+            set_id = ?,
+            official_type = ?
+        WHERE match_official_id = ?;
     `,
 }
 
