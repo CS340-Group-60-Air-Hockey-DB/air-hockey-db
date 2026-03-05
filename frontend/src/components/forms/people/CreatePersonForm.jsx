e15e3686f86e7330ec0de3ac4c5a210c546b0552
Alex <alexandriaduell@gmail.com>
2026-03-04 19:02:30 -0500

    Updated styling for add new person form modal

--- frontend/src/components/forms/people/CreatePersonForm.jsx
+++ frontend/src/components/forms/people/CreatePersonForm.jsx
@@ -2,101 +2,185 @@ const CreatePersonForm = ({ backendURL }) => {
 
     return (
         <div className="modals">
-            <h2>Add a New Person</h2>
+            <div id="modal-header">
+                <h2>
+                    Add New Person
+                </h2>
+
+                <p>
+                    Fill in the details below. Any field with <span className="asterisk">*</span> are required.
+                </p>
+            </div>
+            
 
             <form className='cuForm'>
-                <label htmlFor="first_name">First Name: </label>
-                <input
-                    type="text"
-                    name="first_name"
-                    id="first_name"
-                    required
-                />
+                {/* Personal Info */}
+                <div className="section">
+                    <p className="section-label">
+                        personal info
+                    </p>
 
-                <label htmlFor="last_name">Last Name: </label>
-                <input
-                    type="text"
-                    name="last_name"
-                    id="last_name"
-                    required
-                />
+                    <div className="form-row">
+                        <label htmlFor="first_name">First Name <span className="asterisk">*</span>
+                            <input
+                                placeholder="Type First Name"
+                                type="text"
+                                name="first_name"
+                                id="first_name"
+                                required
+                                autoFocus
+                            />
+                        </label>
 
-                <label htmlFor="gender">Gender: </label>
-                <select
-                    name="gender"
-                    id="gender"
-                >
-                    <option value="prefer not to say">Prefer not to say</option>
-                    <option value="female">Female</option>
-                    <option value="male">Male</option>
-                    <option value="other">Other</option>
-                </select>
+                        <label htmlFor="last_name">Last Name <span className="asterisk">*</span> 
+                            <input
+                                placeholder="Type Last Name"
+                                type="text"
+                                name="last_name"
+                                id="last_name"
+                                required
+                            />
+                        </label>
+                    </div>
 
-                <label htmlFor="dob">Date of Birth: </label>
-                <input
-                    type="date"
-                    name="dob"
-                    id="dob"
-                />
+                    <div className="form-row">
+                        <label htmlFor="gender">Gender
+                            <select
+                                name="gender"
+                                id="gender"
+                            >
+                                <option value="prefer not to say">Prefer not to say</option>
+                                <option value="female">Female</option>
+                                <option value="male">Male</option>
+                                <option value="other">Other</option>
+                            </select>
+                        </label>
 
-                <label htmlFor="email">Email: </label>
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                />
+                        <label htmlFor="dob">Date of Birth <span className="asterisk">*</span>
+                            <input
+                                type="date"
+                                name="dob"
+                                id="dob"
+                                required
+                            />
+                        </label>
+                    </div>
+                </div>
 
-                <label htmlFor="phone_num">Phone: </label>
-                <input
-                    type="tel"
-                    name="phone_num"
-                    id="phone_num"
-                />
+                {/* Contact */}
+                <div className="section">
+                    <p className="section-label">
+                        contact
+                    </p>
 
-                <label htmlFor="street_address_1">Address: </label>
-                <input
-                    type="text"
-                    name="street_address_1"
-                    id="street_address_1"
-                />
+                    <div className="form-row">
+                        <label htmlFor="email">Email
+                            <input
+                                placeholder="Type Email"
+                                type="email"
+                                name="email"
+                                id="email"
+                            />
+                        </label>
 
-                <label htmlFor="street_address_2">Address 2: </label>
-                <input
-                    type="text"
-                    name="street_address_2"
-                    id="street_address_2"
-                />
+                        <label htmlFor="phone_num">Phone
+                            <input
+                                placeholder="(xxx) xxx - xxxx"
+                                type="tel"
+                                name="phone_num"
+                                id="phone_num"
+                            />
+                        </label>
+                    </div>
+                </div>
 
-                <label htmlFor="city">City: </label>
-                <input
-                    type="text"
-                    name="city"
-                    id="city"
-                />
+                {/* Address */}
+                <div className="section">
+                    <p className="section-label">
+                        address
+                    </p>
+
+                    <div className="form-row">
+                        <label 
+                            className="address-line"
+                            htmlFor="street_address_1"
+                        >
+                            Address
+                            
+                            <input
+                                placeholder="Type Street Address (Line 1)"
+                                type="text"
+                                name="street_address_1"
+                                id="street_address_1"
+                            />
+                        </label>
+                    </div>
+
+                    <div className="form-row">
+                        <label 
+                            className="address-line"
+                            htmlFor="street_address_2"
+                        >
+                                
+                            Address Line 2
+                            
+                            <input
+                                placeholder="Type Street Address (Line 2)"
+                                type="text"
+                                name="street_address_2"
+                                id="street_address_2"
+                            />
+                        </label>
+                    </div>
+
+                    <div className="form-row">
+                        <label htmlFor="city">City
+                            <input
+                                placeholder="Type City"
+                                type="text"
+                                name="city"
+                                id="city"
+                            />
+                        </label>
+
+                        <label htmlFor="state">State  
+                            <input
+                                placeholder="Type State"
+                                type="text"
+                                name="state"
+                                id="state"
+                            />
+                        </label>
+                    </div>
+
+                    <div className="form-row">
+                        <label htmlFor="country">Country <span className="asterisk">*</span>  
+                            <input
+                                placeholder="Type Country"
+                                type="text"
+                                name="country"
+                                id="country"
+                                defaultValue="USA"
+                                required
+                            />
+                        </label>
+
+                        <label htmlFor="zip_code">Zip Code    
+                            <input
+                                placeholder="Type Zip Code"
+                                type="text"
+                                name="zip_code"
+                                id="zip_code"
+                            />
+                        </label>
+                    </div>
+                </div>
             
-                <label htmlFor="state">State: </label>  
-                <input
-                    type="text"
-                    name="state"
-                    id="state"
-                />
-
-                <label htmlFor="country">Country: </label>  
-                <input
-                    type="text"
-                    name="country"
-                    id="country"
-                    defaultValue="USA"
-                />
-
-                <label htmlFor="zip_code">Zip Code: </label>    
-                <input
-                    type="text"
-                    name="zip_code"
-                    id="zip_code"
-                />
-            
-                <input type="submit" value="Add Person" />
+                <div id="modal-btn-row">
+                    <button type="submit" value="Add Person">
+                        Add Person
+                    </button>
+                </div>
             </form>
         </div>
     );
