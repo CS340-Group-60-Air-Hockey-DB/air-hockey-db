0c4d8bc065c17f4cbe96c40c28d5a80d825da6e0
Alex <alexandriaduell@gmail.com>
2026-03-05 19:44:43 -0500

    Added add person endpoint (POST) to people routes

--- backend/controllers/people.js
+++ backend/controllers/people.js
@@ -2,6 +2,46 @@
 const db = require('../database/db-connector');
 const people_queries = require('../database/queries/people')
 
+const add_person = async (req, res) => {
+    const { first_name, last_name, gender, dob, email, phone_num,
+        street_address_1, street_address_2, city, state,
+        country, zip_code } = req.body
+
+    const sp_query = `CALL sp_add_person (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, @person_id, @error_message);
+                        SELECT @person_id, @error_message;`
+
+    const values = [first_name, last_name, gender, dob, email, phone_num, street_address_1, street_address_2, city, state, country, zip_code]
+
+    try {
+        const add_res = await db.query(sp_query, values);
+
+        if (add_res[0][1][0]['@error_message'] !== null) {
+            return res.status(400).json({
+                message: `There was an error with adding the person.`,
+                error: add_res[0][1][0]['@error_message']
+            })
+        }
+
+        return res.status(201).json({
+            message: "Person added successfully",
+            person_id: add_res[0][1][0]['@person_id']
+        });
+    }
+    catch (error) {
+        if (error.sqlState === "45000") {
+            return res.status(400).json({
+                message: `An error occurred while adding the person due to the request body.`,
+                error
+            })
+        }
+
+        return res.status(500).send({
+            message: "An error occurred while adding the match.",
+            error
+        });
+    }
+}
+
 
 const get_all_people = async (req, res) => {
     try {
@@ -18,5 +58,6 @@ const get_all_people = async (req, res) => {
 
 
 module.exports = {
+    add_person,
     get_all_people
 }
