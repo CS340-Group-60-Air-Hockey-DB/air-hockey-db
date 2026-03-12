f24f04b7acf2b443238b9d2041d119f6430c81ec
Alex <alexandriaduell@gmail.com>
2026-03-11 23:33:25 -0400

    Renamed person state for the form input data; took out commented out code; added in comments

--- frontend/src/components/forms/people/UpdatePersonForm.jsx
+++ frontend/src/components/forms/people/UpdatePersonForm.jsx
@@ -3,8 +3,10 @@ import { useState } from 'react';
 const UpdatePersonForm = (props) => {
     const { backendURL, people, refreshData, setUpdateModal } = props
 
-    const [updatePerson, setUpdatePerson] = useState({});
+    const [person, setPerson] = useState({});
 
+    // Sets the state for all inputs
+    // Splits the date of birth so the date can be automatically inputted for the date input
     const handleSelectPerson = evt => {
         let person_id = parseInt(evt.target.value)
         let person = people.find(p => p.person_id === person_id)
@@ -80,7 +82,7 @@ const UpdatePersonForm = (props) => {
                                 name="update_person_id"
                                 id="update_person_id"
                                 required
-                                value={updatePerson?.person_id}
+                                value={person?.person_id}
                                 onChange={handleSelectPerson}
                             >
                                 <option value="">
@@ -101,7 +103,7 @@ const UpdatePersonForm = (props) => {
                     </div>
                     
                 {
-                    Object.keys(updatePerson).length > 0 && 
+                    Object.keys(person).length > 0 && 
                     <>
                         {/* Personal Info */}
                         <div className="section">
@@ -120,7 +122,7 @@ const UpdatePersonForm = (props) => {
                                     id="first_name"
                                     required
                                     autoFocus
-                                    value={updatePerson.first_name ?? ''}
+                                    value={person.first_name ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -134,7 +136,7 @@ const UpdatePersonForm = (props) => {
                                     name="last_name"
                                     id="last_name"
                                     required
-                                    value={updatePerson.last_name ?? ''}
+                                    value={person.last_name ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -145,7 +147,7 @@ const UpdatePersonForm = (props) => {
                                 <select
                                     name="gender"
                                     id="gender"
-                                    value={updatePerson.gender ?? ''}
+                                    value={person.gender ?? ''}
                                     onChange={handleInputOnChange}
                                 >
                                     <option value=''> Select a Gender </option>
@@ -162,7 +164,7 @@ const UpdatePersonForm = (props) => {
                                     name="dob"
                                     id="dob"
                                     required
-                                    value={updatePerson.dob ?? ''}
+                                    value={person.dob ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -182,12 +184,13 @@ const UpdatePersonForm = (props) => {
                                     type="email"
                                     name="email"
                                     id="email"
-                                    value={updatePerson.email ?? ''}
+                                    value={person.email ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
 
-                            <label htmlFor="phone_num">Phone Number (ex: 123-456-7890)
+                            <label htmlFor="phone_num">
+                                Phone Number (ex: 123-456-7890)
                                 <input
                                     placeholder="xxx-xxx-xxxx"
                                     pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
@@ -196,7 +199,7 @@ const UpdatePersonForm = (props) => {
                                     type="tel"
                                     name="phone_num"
                                     id="phone_num"
-                                    value={updatePerson.phone_num ?? ''}
+                                    value={person.phone_num ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -221,7 +224,7 @@ const UpdatePersonForm = (props) => {
                                     type="text"
                                     name="street_address_1"
                                     id="street_address_1"
-                                    value={updatePerson.street_address_1 ?? ''}
+                                    value={person.street_address_1 ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -240,7 +243,7 @@ const UpdatePersonForm = (props) => {
                                     type="text"
                                     name="street_address_2"
                                     id="street_address_2"
-                                    value={updatePerson.street_address_2 ?? ''}
+                                    value={person.street_address_2 ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -253,7 +256,7 @@ const UpdatePersonForm = (props) => {
                                     type="text"
                                     name="city"
                                     id="city"
-                                    value={updatePerson.city ?? ''}
+                                    value={person.city ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -264,7 +267,7 @@ const UpdatePersonForm = (props) => {
                                     type="text"
                                     name="state"
                                     id="state"
-                                    value={updatePerson.state ?? ''}
+                                    value={person.state ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -278,7 +281,7 @@ const UpdatePersonForm = (props) => {
                                     name="country"
                                     id="country"
                                     required
-                                    value={updatePerson.country ?? 'USA'}
+                                    value={person.country ?? 'USA'}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -289,7 +292,7 @@ const UpdatePersonForm = (props) => {
                                     type="text"
                                     name="zip_code"
                                     id="zip_code"
-                                    value={updatePerson.zip_code ?? ''}
+                                    value={person.zip_code ?? ''}
                                     onChange={handleInputOnChange}
                                 />
                             </label>
@@ -312,119 +315,3 @@ const UpdatePersonForm = (props) => {
 };
 
 export default UpdatePersonForm;
-
-// <label htmlFor="update_last_name">Last Name: </label>
-                        // <input
-                        //     type="text"
-                        //     name="last_name"
-                        //     id="update_last_name"
-                        //     required
-                        //     value={updatePerson?.last_name || ''}
-                        // />
-
-                        // <label htmlFor="update_gender">Gender: </label>
-                        // <select
-                        //     name="gender"
-                        //     id="update_gender"
-                        // >
-                        //     <option value="prefer not to say">Prefer not to say</option>
-                        //     <option value="female">Female</option>
-                        //     <option value="male">Male</option>
-                        //     <option value="other">Other</option>
-                        // </select>
-
-                        // <label htmlFor="update_dob">Date of Birth: </label>
-                        // <input
-                        //     type="date"
-                        //     name="dob"
-                        //     id="update_dob"
-                        //     value={updatePerson?.dob || ''}
-
-                        // />
-
-                        // {/* Contact Info */}
-                        // <div className="section">
-                        //     <p className="section-label">
-                        //         personal info
-                        //     </p>
-
-                        //     <div className="form-row">
-
-                        //     </div>
-                        // </div>
-
-                        // <label htmlFor ="update_email">Email: </label>
-                        // <input
-                        //     type="email"
-                        //     name="email"
-                        //     id="update_email"
-                        //     value={updatePerson?.email || ''}
-                        // />
-
-                        // <label htmlFor="update_phone_num">Phone: </label>
-                        // <input
-                        //     type="tel"
-                        //     name="phone_num"
-                        //     id="update_phone_num"
-                        //     value={updatePerson?.phone_num || ''}
-                        // />
-
-                        // {/* Address */}
-                        // <div className="section">
-                        //     <p className="section-label">
-                        //         personal info
-                        //     </p>
-
-                        //     <div className="form-row">
-
-                        //     </div>
-                        // </div>
-                        // <label htmlFor="update_street_address_1">Address: </label>
-                        // <input
-                        //     type="text"
-                        //     name="street_address_1"
-                        //     id="update_street_address_1"
-                        //     value={updatePerson?.street_address_1 || ''}
-                        // />
-
-                        // <label htmlFor="update_street_address_2">Address Line 2: </label>
-                        // <input
-                        //     type="text"
-                        //     name="street_address_2"
-                        //     id="update_street_address_2"
-                        //     value={updatePerson?.street_address_2 || ''}
-                        // />
-
-                        // <label htmlFor="update_city">City: </label>
-                        // <input
-                        //     type="text"
-                        //     name="city"
-                        //     id="update_city"
-                        //     value={updatePerson?.city || ''}
-                        // />
-
-                        // <label htmlFor="update_state">State: </label>
-                        // <input
-                        //     type="text"
-                        //     name="state"
-                        //     id="update_state"
-                        //     value={updatePerson?.state || ''}
-                        // />
-
-                        // <label htmlFor="update_country">Country: </label>
-                        // <input
-                        //     type="text"
-                        //     name="country"
-                        //     id="update_country"
-                        //     value={updatePerson?.country || ''}
-                        // />
-
-                        // <label htmlFor="update_zip_code">Zip Code: </label>
-                        // <input
-                        //     type="text"
-                        //     name="zip_code"
-                        //     id="update_zip_code"
-                        //     value={updatePerson?.zip_code || ''}
-                        // />
-
-                        // <input type="submit" value="Update Person"/>
