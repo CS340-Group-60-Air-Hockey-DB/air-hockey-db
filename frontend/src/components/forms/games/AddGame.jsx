e287bf4089dde1b66f9a64d117d2aee632e39072
Alex <alexandriaduell@gmail.com>
2026-03-14 02:16:21 -0400

    Updated add game component + form modal header section styling; rearranged add game component form sections

--- frontend/src/components/forms/games/AddGame.jsx
+++ frontend/src/components/forms/games/AddGame.jsx
@@ -27,12 +27,24 @@ function AddGame(props) {
                     </button>
 
                     <h2>Add a New Game</h2>
+                </div>
 
+                <div 
+                    id='modal-p-div'
+                    className='modal-p-div-overflow'
+                >
                     <p>
                         Fill in the details below by scrolling to add a new game to any match.
                     </p>
+
                     <p>
-                        Any fields with <span className="asterisk">*</span> are required.
+                        If you do not see a match, all games have been added for all sets. Please use the update button if you need to change a game&apos;s details.
+                    </p>
+
+                    <p>
+                        Any fields with 
+                        <span className="asterisk"> * </span> 
+                        are required.
                     </p>
                 </div>
 
@@ -43,64 +55,127 @@ function AddGame(props) {
                     // Disable the forms auto validation for scrolling in the handleSubmit function above
                     noValidate
                 >
+                    <div className="section">
+                        <div className="form-row init-select">
+                            <label 
+                                htmlFor="match_id"
+                                className='section-label'
+                            >
+                                Select a match
+                                <span className="asterisk">*</span>
+                            </label>
 
-                    <label> Match: </label>
-                        <select 
-                            required
-                            onChange={evt => {
-                                setMatchNum(evt.target.value[0])
-                                setSetMax(Array.from({ length: evt.target.value[2]}, (_, idx) => idx + 1))
-                            }}
-                        >
-                            <option value="">Select a Match</option>
-                            {matches.map(m => 
-                                <option 
-                                    key={m.match_id} 
-                                    value={[m.match_id, m.set_max]}
-                                >
-                                    {m.match_id}
+                            <select 
+                                required
+                                onChange={handleSelectMatch}
+                            >
+                                <option value="">
+                                    To add a game, select a match
                                 </option>
-                            )}
-                        </select>
+                                {
+                                    matches.map(m => 
+                                        <option 
+                                            key={m.match_id} 
+                                            value={m.match_id}
+                                        >
+                                            {m.match_id}
+                                        </option>
+                                    )
+                                }
+                            </select>
+                        </div>
+                    </div>
 
                     {
-                        matchNum && setMax && 
-                        <div>
-                            <label> Set: </label>
-                                <select required>
-                                    <option value="">{'Select a Set'}</option>
-                                    {setMax.map((num, idx) => <option key={idx} value={num}>{num}</option>)}
-                                </select>
-                        </div>
+                        matchId && 
+                        <>
+                            {/* Select Set + Game Number */}
+                            <div className="section">
+                                <div className="form-row">
+                                    <label> 
+                                        Set 
+                                    
+                                        <select required>
+                                            <option value="">{'Select a Set'}</option>
+                                            {setArr.map((num, idx) => <option key={idx} value={num}>{num}</option>)}
+                                        </select>
+                                    </label>
+
+                                    <label>
+                                        Game Number 
+                                    
+                                        <select required>
+                                            <option value="">{'Select the Game Number'}</option>
+                                                {gameNumArr.map((num, idx) => <option key={idx} value={num}>{num}</option>)}
+                                        </select>
+                                    </label>
+                                </div>
+                            </div>
+
+                            {/* Player Scores */}
+                            <div className="section">
+                                <div className="form-row">
+                                    <label
+                                        htmlFor='player_1_score'
+                                    >
+                                        Player 1 Score
+                                    
+                                        <input 
+                                            type="number" 
+                                            name='player_1_score'
+                                            placeholder='Enter Score'
+                                            min="0" 
+                                            max="7"
+                                            required 
+                                        />
+                                    </label>
+
+                                    <label
+                                        htmlFor='player_2_score'
+                                    >
+                                        Player 2 Score
+
+                                        <input 
+                                            type="number" 
+                                            name='player_2_score'
+                                            placeholder='Enter Score'
+                                            min="0" 
+                                            max="7"
+                                            required 
+                                        />
+                                    </label>
+                                </div>
+                            </div>
+
+                            {/* Game Status */}
+                            <div className="section">
+                                <div className="form-row">
+                                    <label> 
+                                        Game Status
+                                    
+                                        <select required>
+                                            <option value="scheduled">Scheduled</option>
+                                            <option value="in_progress">In Progress</option>
+                                            <option value="completed">Completed</option>
+                                        </select>
+                                    </label>
+                                </div>
+                            </div>
+                    
+                            <div id="modal-btn-row">
+                                <button 
+                                    type="submit"
+                                >
+                                    Add Game
+                                </button>
+                            </div>
+                        </>
                     }
 
-                    <label>Game Number: </label>
-                        <select required>
-                            <option value="">{'Select the Game Number'}</option>
-                                {gameNumArr.map((num, idx) => <option key={idx} value={num}>{num}</option>)}
-                            </select>
-
-                    <br /><br />
-
-                    <label>Player 1 Score: </label>
-                    <input 
-                        type="number" 
-                        min="0" 
-                        max="7"
-                        required 
-                    />
-
-                    <label>Player 2 Score:</label>
-                    <input type="number" min="0" max="7"required />
-
-                    <label> Status: </label>
-                    <select required>
-                        <option value="scheduled">Scheduled</option>
-                        <option value="in_progress">In Progress</option>
-                        <option value="completed">Completed</option>
-                    </select>
-
-                    <button type="submit">Add Game</button>
+                    {/* If sets cannot be found for the match with the selected id */}
+                    {/* {
+                        matchId && setArr.length === 0 && alert(`Match ${matchId} and/or the set max could not be found. Please refresh and try again.`)
+                    } */}
                 </form>  
             </div>
         </div>
