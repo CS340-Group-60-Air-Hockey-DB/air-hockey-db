4a7bf66e0112d296340f5df66f4466fd65b48157
Alex <alexandriaduell@gmail.com>
2026-03-14 17:22:03 -0400

    Updated create location controller for a safe person id when 'no owner' is passed; updated error messages logic

--- backend/controllers/locations.js
+++ backend/controllers/locations.js
@@ -28,6 +28,9 @@ const create_location = async (req, res) => {
 
         const safe_email = email === '' ? null : email
         const safe_note = note === "" ? null : note;
+        // After : is for when person_id = 'no owner'
+        // To make it null instead of NaN
+        const safe_person_id = person_id === '' ? null : parseInt(person_id) || null
 
         const query = `CALL sp_add_location(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, @insertId, @error_message);
                         SELECT @insertId, @error_message;`;
@@ -39,31 +42,50 @@ const create_location = async (req, res) => {
 
         const [result] = await db.query(query, values);
 
-        if (result[1][0]['@error_message'] !== null) {
-            const message = result[1][0]['@error_message'] && result[1][0]['@error_message'].includes('unique_address')
-                ? 'The location provided is already taken. Either update that location or provide another address.' : 'There was an error adding the location.'
+        let message
 
-            return res.status(400).json({
-                error: result[1][0]['@error_message'],
-                message
-            })
+        if (result[1][0]['@error_message'] !== null) {
+            if (result[1][0]['@error_message'].includes('unique_address')) {
+                message = 'The location provided is already taken. Either update that location or provide another address.'
+
+                return res.status(400).json({
+                    error: result[1][0]['@error_message'],
+                    message
+                })
+            }
+            else if (result[1][0]['@error_message'].includes('email')) {
+                message = 'The email provided is already taken. Please provide another email address.'
+
+                return res.status(400).json({
+                    error: result[1][0]['@error_message'],
+                    message
+                })
+            }
+            else {
+                return res.status(400).json({
+                    error: result[1][0]['@error_message'],
+                    message: 'There was an error adding the location.'
+                })
+            }
         }
 
         const newLocationId = result[1][0]['@insertId'];
 
         // insert junction table record if owner was selected
-        if (person_id) {
+        if (safe_person_id) {
             const query2 = `
                 INSERT INTO people_locations (person_id, location_id)
                 VALUES (?, ?);
             `
 
-            await db.query(query2, [person_id, newLocationId]);
+            await db.query(query2, [safe_person_id, newLocationId]);
         }
 
         res.status(201).json({ message: "Location created successfully!", id: result.insertId });
 
     } catch (error) {
+        console.error("Error updating location:", error);
+
         if (error.sqlState === '23000') {
             if (error.sqlMessage?.includes('people_locations')) {
                 return res.status(400).json({
