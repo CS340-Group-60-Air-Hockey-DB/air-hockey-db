6e2f7e471939a3f0f160d61b8079a9e7dbfc2f4c
Alex <alexandriaduell@gmail.com>
2026-03-16 22:31:53 -0400

    Added triggers to reset database endpoint

--- backend/controllers/reset.js
+++ backend/controllers/reset.js
@@ -1,12 +1,15 @@
 const db = require('../database/db-connector');
+const all_triggers = require('../database/triggers/all_triggers');
 
-const reset_database = (req, res) => {
-    const query = `CALL sp_reset_db();`;
+const reset_database = async (req, res) => {
+    const query = `CALL sp_reset_db();
+        ${all_triggers}
+    `;
 
-    db.pool.query(query, (error, results) => {
+    db.pool.query(query, async (error, results) => {
         if (error) {
             console.error("Error executing ResetDB procedure:", error);
-            res.status(500).json({ error: "Failed to reset database."});
+            res.status(500).json({ error: "Failed to reset database." });
         } else {
             console.log("Database reset successful.");
             res.status(200).json({ message: "Database reset successfully!" });
